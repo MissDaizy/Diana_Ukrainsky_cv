c61dc5a8747606bca201bec77e4d0a3bf85439b8
MissDaizy <dianaukrainsky@gmail.com>
2023-01-01 21:20:56 +0200

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.scss";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
