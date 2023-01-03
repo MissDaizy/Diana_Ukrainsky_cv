71987ae8e2d315b0d5895167023a35bfbe366009
MissDaizy <dianaukrainsky@gmail.com>
2023-01-03 13:15:20 +0200

    Finished template for cv.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
 import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
